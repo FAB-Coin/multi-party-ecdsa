f41608ec4eee6f0814bebb3700a22908389199cf
APIS Project <53349926+APIS-Project@users.noreply.github.com>
2019-08-25 20:03:45 +0300

    src

---
+++ src/lib.rs
@@ -0,0 +1,35 @@
+/*
+    Multi-party ECDSA
+
+    Copyright 2018 by Kzen Networks
+
+    This file is part of Multi-party ECDSA library
+    (https://github.com/KZen-networks/multi-party-ecdsa)
+
+    Multi-party ECDSA is free software: you can redistribute
+    it and/or modify it under the terms of the GNU General Public
+    License as published by the Free Software Foundation, either
+    version 3 of the License, or (at your option) any later version.
+
+    @license GPL-3.0+ <https://github.com/KZen-networks/multi-party-ecdsa/blob/master/LICENSE>
+*/
+
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+extern crate serde_json;
+extern crate subtle;
+
+extern crate centipede;
+extern crate curv;
+extern crate paillier;
+extern crate zk_paillier;
+pub mod protocols;
+
+#[derive(Copy, PartialEq, Eq, Clone, Debug)]
+pub enum Error {
+    InvalidKey,
+    InvalidSS,
+    InvalidCom,
+    InvalidSig,
+}
